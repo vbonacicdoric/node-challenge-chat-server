388abdee37db996130fdedc9b970a766eececb59
Valeria Bonacic Doric <94327965+vbonacicdoric@users.noreply.github.com>
2022-06-14 20:08:36 +0200

    desafio 1 falta seguir completando

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+
+// AUX FUNCTIONS
 const express = require("express");
 const cors = require("cors");
-
+const { request } = require("express");
 const app = express();
-
 app.use(cors());
-
 const welcomeMessage = {
   id: 0,
   from: "Bart",
@@ -14,12 +14,54 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
 
+
+  // FUNCTIONS
+const messages = [welcomeMessage];
+function saveMessage(request, response) {
+  const newMessage = request.body;
+  const newId = messages.length;
+  newMessage.id = newId;
+  messages.push(newMessage);
+  response.status(201).send(newMessage);
+}
+function getMessageById(req, res){
+  const messageId = Number(req.params.messageId);
+    const message = messages.find((message) => message.id === messageId);
+    res.send(message);
+  }
+
+
+
+// MIDDLEWARES
+app.use(express.json());
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+app.get("/messages",(req, res) => { 
+  res.send(messages)
+});
+app.post("/messages" , saveMessage);
+app.get("/messages/:id", getMessageById);  
+app.delete("/messages/:messageId", (req, res) => {
+        const messageId = Number(req.params.messageId);
+        messages.splice(messageId, 1);
+        res.send(messages);
+      });
 
-app.listen(3000, () => {
-   console.log("Listening on port 3000")
-  });
+
+
+
+// SERVER
+const port = 3000;
+const url = `http://localhost:${port}/messages`;
+app.listen(port, () => console.log(`Listening on port ${url}`));
+
+
+
+
+              
+              // const getAllMessages = (req, res) => {
+              // res.send(messages)
+              // };
+              
