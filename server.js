d61fc1cb062ec46b0ca67099da96629417a13f66
Valeria Bonacic Doric <94327965+vbonacicdoric@users.noreply.github.com>
2022-06-20 20:16:32 +0200

    tarea hecha

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // AUX FUNCTIONS
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 const { request } = require("express");
 const app = express();
 app.use(cors());
@@ -10,14 +11,15 @@ const welcomeMessage = {
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
+const file = "messagesData.json";
+const messages = [welcomeMessage];
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 
-
   // FUNCTIONS
-const messages = [welcomeMessage];
+
 function saveMessage(request, response) {
   const newMessage = request.body;
   const newId = messages.length;
@@ -25,13 +27,19 @@ function saveMessage(request, response) {
   messages.push(newMessage);
   response.status(201).send(newMessage);
 }
+const searchMessage = (req, res) => {
+  const text = req.query.text.toLowerCase();
+  const findText = messages.filter((message) =>
+    message.text.toLowerCase().includes(text)
+  );
+
+  res.send(findText);
+};
 function getMessageById(req, res){
   const messageId = Number(req.params.messageId);
     const message = messages.find((message) => message.id === messageId);
     res.send(message);
   }
-
-
 
 // MIDDLEWARES
 app.use(express.json());
@@ -42,6 +50,7 @@ app.get("/messages",(req, res) => {
   res.send(messages)
 });
 app.post("/messages" , saveMessage);
+app.get("/messages/search", searchMessage);
 app.get("/messages/:id", getMessageById);  
 app.delete("/messages/:messageId", (req, res) => {
         const messageId = Number(req.params.messageId);
@@ -49,19 +58,24 @@ app.delete("/messages/:messageId", (req, res) => {
         res.send(messages);
       });
 
-
-
+app.put("/messages/:id", (req, res) => {
+        const id = Number(req.params.id)
+        const message = messages.find(message => id === message.id)
+        if (message) {
+          message.text = req.body.text;
+          message.from = req.body.from;
+          res.send({
+            status: 'Your message has been modified!',
+            data: message
+          });
+        } else {
+          res.status(400);
+          res.send('Your message has not been modified!');
+        }
+      })
+    
 
 // SERVER
 const port = 3000;
 const url = `http://localhost:${port}/messages`;
 app.listen(port, () => console.log(`Listening on port ${url}`));
-
-
-
-
-              
-              // const getAllMessages = (req, res) => {
-              // res.send(messages)
-              // };
-              
